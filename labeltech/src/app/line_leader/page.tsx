acd8f237201914221ab8822fcc426148e20d8809
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-11-03 23:16:03 +0000

    line leader page developement

--- labeltech/src/app/line_leader/page.tsx
+++ labeltech/src/app/line_leader/page.tsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Line leader</div>
+    <div className='text-3xl'>
+      <div className='flex justify-end w-full mt-10'>
+        <p className='mr-5'>Today's date</p>
+        <p className='greyinput mr-16'>29/11/2023</p>
+      </div>
+      <div className='flex justify-around py-5'>
+        <div className='border-solid border-2 border-black rounded-lg w-5/12 py-5 px-3'>
+          <div className='flex flex-raw py-5'>
+            <p>Select product code</p>
+            <p className='input ml-5'>532</p>
+          </div>
+          <div className='flex flex-raw py-5'>
+            <p>Select depot date</p>
+            <p className='input ml-5'>30/11/2023</p>
+          </div>
+          <div className='flex flex-raw py-5'>
+            <p className='pt-2'>Confirm details before scanning</p>
+            <button className='greenbtn'>Confirm</button>
+          </div>
+        </div>
+        <div className='border-solid border-2 border-black rounded-lg w-5/12 py-5 px-3'>
+          <p className='text-center mb-5'>Scanning in progress</p>
+          <div className='flex flex-raw py-5'>
+            <p>Packing error</p>
+            <p className='input ml-5'>0</p>
+          </div>
+          <div className='flex flex-raw py-5'>
+            <p>Packed without error</p>
+            <p className='input ml-5'>0</p>
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-center mt-16'>
+        <p className='mr-10 pt-2'>Start scanning</p>
+        <button className='greenbtn'>Start</button>
+      </div>
+    </div>
   )
 }
 
