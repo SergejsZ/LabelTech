986f0deff7aaa7777d9d34494cd1463d5d0d7726
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-11-07 14:02:13 +0000

    Update ProductionLineManagement.tsx

--- labeltech/src/components/ProductionLineManagement.tsx
+++ labeltech/src/components/ProductionLineManagement.tsx
@@ -7,6 +7,24 @@ const ProductionLineManagement = () => {
     <div>
         <h2 className='text-center font-bold'>Production Line Management</h2>
         <div className='flex flex-wrap mt-5'>
+            <div className='flex flex-col border-solid border-2 border-black rounded m-2'>
+                    <h2 className='text-center'>Edit production line</h2>
+                    <div className='pl-2 flex flex-raw mx-5'>
+                        <div className='flex flex-col'>
+                            <p className='mr-2 py-2'>Line</p>
+                            <p className='mr-2 py-2'>Username</p>
+                            <p className='mr-2 py-3'>Password</p>
+
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='basicinput'>2</p>
+                            <p className='basicinput'>9018</p>
+                            <p className='basicinput'>....</p>                            
+                        </div>
+                            
+                    </div>
+                    <button className='greenbtn mx-30pc my-5'>Save</button>
+            </div>
             <div className='m-2 w-32'>
                 <ul>
                     {lines.map((line) => (
