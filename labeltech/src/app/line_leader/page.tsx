93e62b8e6fa4dfbfb7f9079e49d70402bf7f5766
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-11-11 21:27:12 +0000

    upgrade line_leader UI

--- labeltech/src/app/line_leader/page.tsx
+++ labeltech/src/app/line_leader/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 const page = () => {
 
@@ -9,52 +11,76 @@ const page = () => {
 
   const currentDate = `${day}/${month}/${year}`;
 
+  const [productCode, setProductCode] = useState('');
+  const [depotDate, setDepotDate] = useState('');
+  const [packingError, setPackingError] = useState(0);
+  const [packedWithoutError, setPackedWithoutError] = useState(0);
+
+  const handleConfirm = () => {
+
+  };
+
+  const handleStartScanning = () => {
+
+  };
+
   return (
     <div className='text-3xl'>
       <div className='flex justify-end w-full mt-10'>
         <p className='mr-5'>Today's date</p>
         <p className='greyinput mr-16'>{currentDate}</p>
       </div>
-      <div className='flex justify-around py-5'>
-      <form className='border-solid border-2 border-black rounded-lg w-5/12 py-5 px-3'>
-        <div className='flex flex-row py-5 items-center'>
-          <label htmlFor="productCode" className='mr-2'>Select product code:</label>
+      <div className="flex justify-between p-5">
+      <div className="flex-1 bg-white shadow-lg rounded-lg p-6 mr-3 text-center">
+        <div className="mb-4">
+          <label htmlFor="productCode" className="block text-lg font-semibold mb-2">Select product code:</label>
           <input 
-            type="text"
-            id="productCode"
-            name="productCode"
-            className='basicinput w-1/2'
-            placeholder='Enter product code'
-            //TODO: add the regex pattern for the product code
+            type="text" 
+            id="productCode" 
+            placeholder="Enter product code" 
+            value={productCode} 
+            onChange={(e) => setProductCode(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
           />
         </div>
-        <div className='flex flex-row py-5 items-center'>
-          <label htmlFor="depotDate" className='mr-2'>Select depot date:</label>
+
+        <div className="mb-6">
+          <label htmlFor="depotDate" className="block text-lg font-semibold mb-2">Select depot date:</label>
           <input 
-            type="text"
-            id="depotDate"
-            name="depotDate"
-            className='basicinput w-1/2'
-            placeholder='DD/MM/YYYY'
-            pattern="\d{2}/\d{2}/\d{4}"
-            title="Date should be in the format DD/MM/YYYY."
+            type="date" 
+            id="depotDate" 
+            value={depotDate} 
+            onChange={(e) => setDepotDate(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
           />
         </div>
-        <div className='flex flex-row py-5 items-center'>
-          <span className='pt-2 mr-4'>Confirm details before scanning:</span>
-          <button type="submit" className='greenbtn'>Confirm</button>
+
+        <button 
+          onClick={handleConfirm} 
+          className="bg-green-500 hover:bg-green-600 text-black font-bold py-2 px-4 rounded"
+        >
+          Confirm
+        </button>
+      </div>
+
+      <div className="flex-1 bg-white shadow-lg rounded-lg p-6 text-center">
+        <div className="text-lg font-semibold mb-4">Scanning in progress</div>
+        
+        <div className="flex justify-between items-center mb-4 p-3 bg-gray-100 border border-gray-300 rounded">
+          <label htmlFor="packingError" className="text-lg font-semibold">Packing error:</label>
+          <span className="text-xl font-bold p-2 bg-gray-200 rounded">{packingError}</span>
         </div>
-      </form>
-      <div className='border-solid border-2 border-black rounded-lg w-5/12 py-5 px-3'>
-        <p className='text-center mb-5'>Scanning in progress</p>
-        <div className='flex flex-raw py-5'>
-          <p>Packing error</p>
-          <p className='basicinput ml-5'>0</p>
+
+        <div className="flex justify-between items-center mb-6 p-3 bg-gray-100 border border-gray-300 rounded">
+          <label htmlFor="packedWithoutError" className="text-lg font-semibold">Packed without error:</label>
+          <span className="text-xl font-bold p-2 bg-gray-200 rounded">{packedWithoutError}</span>
         </div>
-        <div className='flex flex-raw py-5'>
-          <p>Packed without error</p>
-          <p className='basicinput ml-5'>0</p>
-        </div>
+
+        <button 
+          onClick={handleStartScanning} 
+          className="bg-green-500 hover:bg-green-600 text-black font-bold py-2 px-4 rounded  ">
+          Start
+        </button>
       </div>
     </div>
     <div className='flex justify-center mt-16'>
