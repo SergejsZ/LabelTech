5bdad1a0b4569ec9f8bd6e5eb1f3325639a20b4c
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-11-23 15:31:56 +0000

    Update ProductionManagement.tsx
    UI product management page

--- labeltech/src/components/ProductionManagement.tsx
+++ labeltech/src/components/ProductionManagement.tsx
@@ -11,12 +11,9 @@ type ProductDetails = {
 const ProductionManagement = () => {
     const productCodes = Array.from({length: 8}, (_, i) => i + 1);
 
-    // State for selected product details
     const [selectedProduct, setSelectedProduct] = useState<ProductDetails | null>(null);
-
-    // State for editing product details
+    const [selectedCode, setSelectedCode] = useState<number | null>(null);
     const [editDetails, setEditDetails] = useState<ProductDetails | null>(null);
-
     const [showOnlyProductList, setShowOnlyProductList] = useState(false);
 
 
@@ -49,6 +46,7 @@ const ProductionManagement = () => {
     const handleProductClick = async (productCode: number) => {
         const details = await fetchProductDetails(productCode);
         setSelectedProduct(details);
+        setSelectedCode(productCode);
     };
 
 
@@ -58,55 +56,56 @@ const ProductionManagement = () => {
                 <h2 className='text-center font-bold'>Product Management</h2>
                 <ul className='m-2 w-32'>
                     {productCodes.map((code) => (
-                        <li key={code} className='border-solid border-black border rounded-lg text-center'>
+                        <li key={code} className={`border-solid border-black border rounded-lg text-center`}>
                             {code}
                         </li>
                     ))}
                 </ul>
-                <button onClick={() => setShowOnlyProductList(!showOnlyProductList)}>
+                {/* <button onClick={() => setShowOnlyProductList(!showOnlyProductList)}>
                     {showOnlyProductList ? "Show Full View" : "Show Only Product List"}
-                 </button>
+                 </button> */}
             </div>
         );
     }
 
     return (
-        <div>
-
+        <div className='h-full'>
             <h2 className='text-center font-bold'>Product Management</h2>
-            <div className='flex flew-raw'>
-                <ul className='m-2 w-32 border-solid border-2 border-black'>
-                    {productCodes.map((code) => (
-                        <li key={code} className='border-solid border-black border rounded-lg text-center' onClick={() => handleProductClick(code)}>
-                            {code}
-                        </li>
-                    ))}
-                </ul>
+            <div className='h-full pt-16'>
+                <div className='flex'>
+                    <ul className='m-2 w-1/4'>
+                        {productCodes.map((code) => (
+                            <li key={code} className={`border-solid border-black border rounded-lg text-center ${code === selectedCode ? 'bg-green-400' : ''}`} onClick={() => handleProductClick(code)}>
+                                {code}
+                            </li>
+                        ))}
+                    </ul>
 
-                {selectedProduct && (
-                    <div className='product-details border-solid border-2 border-black'>
-                        <div className='border-solid border-2 border-black'>
-                            <p>Code: {selectedProduct.code}</p>
-                            <p>Name: {selectedProduct.name}</p>
-                            <p>Description: {selectedProduct.description}</p>
-                            <p>Weight: {selectedProduct.weight}g</p>
-                            <h3>Edit Product Details</h3>
+                    {selectedProduct && (
+                        <div className='product-details border-solid border-2 rounded-lg border-black flex justify-around w-2/3 ml-16'>
+                            <div>
+                                <p className='p-3'>Code: {selectedProduct.code}</p>
+                                <p className='p-3'>Name: {selectedProduct.name}</p>
+                                <p className='p-3'>Description: {selectedProduct.description}</p>
+                                <p className='p-3'>Weight: {selectedProduct.weight}g</p>
+                            </div>
+                            <div> 
+                                <h2 className='font-bold text-center'>Edit product detail</h2>
+                                <form onSubmit={handleEditSubmit} className='flex flex-col'>
+                                    <input type="text" name="name" className='p-3' value={editDetails?.name || ''} onChange={handleEditChange} placeholder="Product Name" />
+                                    <input type="text" name="description" className='p-3' value={editDetails?.description || ''} onChange={handleEditChange} placeholder="Product Description" />
+                                    <input type="number" name="weight" className='p-3' value={editDetails?.weight || 0} onChange={handleEditChange} placeholder="Product Weight" />
+                                    <button className='greenbtn m-1' type="submit">Save Changes</button>
+                                </form>
+                            </div>
                         </div>
-                        <div className='border-solid border-2 border-black'> 
-                            <form onSubmit={handleEditSubmit}>
-                                <input type="text" name="name" value={editDetails?.name || ''} onChange={handleEditChange} placeholder="Product Name" />
-                                <input type="text" name="description" value={editDetails?.description || ''} onChange={handleEditChange} placeholder="Product Description" />
-                                <input type="number" name="weight" value={editDetails?.weight || 0} onChange={handleEditChange} placeholder="Product Weight" />
-                                <button className='greenbtn m-1' type="submit">Save Changes</button>
-                            </form>
-                        </div>
-                    </div>
-                )}
+                    )}
+                </div>
+
+                {/* <button onClick={() => setShowOnlyProductList(!showOnlyProductList)}>
+                    {showOnlyProductList ? "Show Full View" : "Show Only Product List"}
+                </button> */}
             </div>
-
-            <button onClick={() => setShowOnlyProductList(!showOnlyProductList)}>
-                {showOnlyProductList ? "Show Full View" : "Show Only Product List"}
-            </button>
         </div>
     );
 };
