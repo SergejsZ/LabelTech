2b4efa6b07baf665bc18bc8890b4fa5da6ea9f97
John Tayag <d00236472@student.dkit.ie>
2023-12-05 14:36:38 +0000

    :bug: Fixed git conflicts

--- labeltech/src/app/page.tsx
+++ labeltech/src/app/page.tsx
@@ -1,64 +1,108 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+
+interface UserData {
+  username: string;
+  // Add more properties as needed
+}
 
 const Home = () => {
-//Adding missing files from previous push
-  const connected = false;
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
+
+  const handleLogin = async (id: string, password: string) => {
+    try {
+      const response = await fetch('http://localhost:4000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Successful login, redirect to the specified route
+        window.location.href = data.redirect;
+      } else {
+        // Login error, display the error message
+        setLoginError(data.error);
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setLoginError('An error occurred while logging in');
+    }
+  };
+
+  const handleLoginClick = () => {
+    // Get input values
+    const idInput = document.getElementById('id') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    // Call the handleLogin function with the input values
+    handleLogin(idInput.value, passwordInput.value);
+  };
 
   return (
-    // if !connected
-    // else the user is redirect to the dashboard page corresponding to his role (admin or line leader)    
     <div className='mt-32'>
-      <div className="container max-w-md mx-auto xl:max-w-3xl h-full flex bg-slate-100 rounded-lg shadow overflow-hidden">
-        <div className="relative hidden xl:block xl:w-1/2 h-full">
+      {/* Login Form */}
+      {isLoginFormVisible && (
+        <div className="container max-w-md mx-auto xl:max-w-3xl h-full flex bg-slate-100 rounded-lg shadow overflow-hidden">
+             <div className="relative hidden xl:block xl:w-1/2 h-full">
           <img
             className="absolute h-auto w-full object-cover"
             src="../utils/images/Logo_lablelTech.png"
             alt="logo labeltech" />
         </div>
-        <div className="w-full xl:w-1/2 p-8">
-          <form method="post" action="#">
-            <h1 className=" text-2xl font-bold">Log in</h1>
-            <div className="mb-4 mt-6">
-              <label
-                className="loginlabel">
-                ID
-              </label>
-              <input
-                className="logininput"
-                id="id"
-                type="text"
-                placeholder="Your ID"/>
-            </div>
-            <div className="mb-6 mt-6">
-              <label
-                className="loginlabel"
-                htmlFor="password">
-                Password
-              </label>
-              <input
-                className="logininput"
-                id="password"
-                type="password"
-                placeholder="Your password"/>
-            </div>
-            <div className="flex w-full mt-8">
-              {/* <button
-                className="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-                type="button">
-                Log in
-              </button> */}
-              <a className="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10 text-center"
-                href="/line_leader">
-                Log in
-              </a>
-            </div>
-          </form>
+          <div className="w-full xl:w-1/2 p-8">
+            <form method="post" action="#">
+              <h1 className="text-2xl font-bold">
+                Log in {userData && userData.username && ` ${userData.username}`}
+              </h1>
+              <div className="mb-4 mt-6">
+                <label className="loginlabel">ID</label>
+                <input
+                  className="logininput"
+                  id="id"
+                  type="text"
+                  placeholder="Your ID"
+                />
+              </div>
+              <div className="mb-6 mt-6">
+                <label className="loginlabel" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="logininput"
+                  id="password"
+                  type="password"
+                  placeholder="Your password"
+                />
+              </div>
+              <div className="flex w-full mt-8">
+                <button
+                  className="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10 text-center"
+                  onClick={handleLoginClick}
+                >
+                  Log in
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
+
+      {/* Error Message */}
+      {loginError && (
+        <div className="mt-4 text-red-500">{loginError}</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
