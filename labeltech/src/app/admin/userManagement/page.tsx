6bbea0002847635b5858a740b1fffaa7ed02a171
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2024-01-22 02:02:18 +0000

    add user management page

--- labeltech/src/app/admin/userManagement/page.tsx
+++ labeltech/src/app/admin/userManagement/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import PageLayout from '@/app/admin/page';
+import Issue from '@/components/Issue';
 
 const page = () => {
   return (
     <PageLayout >
-    <div>
+    <div className='ml-96 mt-10 w-9/12'>
       <p>users managment</p>
+      <Issue />
     </div>
     </PageLayout >
   );
