27e813d0a78ce974d8b9b4bd4a23efb433491944
natha <d00236171@student.dkit.ie>
2024-02-27 15:33:50 +0000

    :bug: Fixed NavBar bug

--- labeltech/src/components/NavBar.tsx
+++ labeltech/src/components/NavBar.tsx
@@ -62,7 +62,7 @@ const StickyNavbar = () => {
   );
  
   return (
-    <div className="max-h-[768px] w-[calc(100%)] overflow-scroll">
+    <div className="max-h-[768px] w-[calc(100%)]">
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4" placeholder={undefined}>
         <div className="flex items-center justify-between text-blue-gray-900">
             <div className="flex space-x-4 items-center">
