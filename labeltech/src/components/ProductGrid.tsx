1e0288cd5427b1e46b29f0b26ae7869f03d45db7
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2024-03-11 22:55:23 +0000

    Update ProductGrid.tsx

--- labeltech/src/components/ProductGrid.tsx
+++ labeltech/src/components/ProductGrid.tsx
@@ -228,7 +228,7 @@ function ProductGrid({ products }: { products: Array<{ productId:number, product
 
     return (
       <div>
-        <div className="relative h-10 w-full min-w-[200px]">
+        <div className="relative h-10 w-2/3 min-w-[200px] mb-8">
             <div
               className="absolute grid w-5 h-5 top-2/4 right-3 -translate-y-2/4 place-items-center text-blue-gray-500">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -238,7 +238,7 @@ function ProductGrid({ products }: { products: Array<{ productId:number, product
               </svg>
             </div>
             <input
-              className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              className="peer h-full w-full rounded-[7px] border border-blue-gray-800 bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               type="text"
               placeholder="  "
               value={searchString}
@@ -442,6 +442,7 @@ function ProductGrid({ products }: { products: Array<{ productId:number, product
         </button>
 
       </div>
+      <br /><br />
     </div>
     );
 }
