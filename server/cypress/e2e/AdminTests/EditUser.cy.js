093290d915f4f4eacbf4c7e446121a0e39fef328
natha <d00236171@student.dkit.ie>
2024-03-12 15:09:09 +0000

    :white_check_mark: Updated Edit User Test

--- server/cypress/e2e/AdminTests/EditUser.cy.js
+++ server/cypress/e2e/AdminTests/EditUser.cy.js
@@ -1,20 +1,18 @@
 /* ==== Test Created with Cypress Studio ==== */
-it('Admin Editing User Test', function() {
+it('Edit User', function() {
   /* ==== Generated with Cypress Studio ==== */
   cy.visit('localhost:3000');
-  cy.get('#id').clear();
+  cy.get('#id').clear('Admin');
   cy.get('#id').type('Admin');
   cy.get('#password').clear('P');
   cy.get('#password').type('Password123!');
   cy.get('form > .flex > .w-full').click();
-  cy.get('[href="/admin/userManagement"] > .flex').click();
-  cy.get(':nth-child(1) > .font-medium > .text-indigo-600 > .h-5 > path').click();
-  cy.get('#userName').clear('Ale');
-  cy.get('#userName').type('Steve');
-  cy.get('#userLevel').select('Admin');
-  cy.get('.shadow-xl').click();
-  cy.get('#userEmail').clear();
-  cy.get('#userEmail').type('steve@gmail.com');
+  cy.get('.mr-4 > .mt-2 > :nth-child(3) > .flex').click();
+  cy.get(':nth-child(4) > .font-medium > .text-blue-500 > .h-5').click();
+  cy.get('#userName').clear('test1');
+  cy.get('#userName').type('test123');
+  cy.get('#userEmail').click();
+  cy.get('#userLevel').select('Quality');
   cy.get('.bg-blue-600').click();
   /* ==== End Cypress Studio ==== */
 });
